7d5f8cbc87a0d79bdd97b0a7b7f777855f980fcb
CARE <acare@la-providence.net>
2020-11-03 17:06:05 +0100

    modif

--- ExerciceFinal/Exercice.php
+++ ExerciceFinal/Exercice.php
@@ -1,6 +1,5 @@
 <?php
     session_start();
-    $_SESSION['connection'];
 ?>
 <html>
     <head>
@@ -9,63 +8,49 @@
         <title>ExerciceFinal</title>
     </head>
     <body>
-        <form method="POST">
-            <label for="password">Nom:</label>
-            <input type="nom" name="nom" id="nom" value="" />
-            <p>
-                <label for="password">Mot de passe:</label>
-                <input type="password" name="password" id="password" value="" />
-                <input type="submit" name="submit" value="connection" />
-            </p>
-        </form>
-    </body>
-</html>
-<?php
-  define('nom','julien');
-  define('password','1234');
-  $errorMessage = 'identifiant ou mots de passe incorect.';
-  if(isset($_POST["submit"])) 
-  {
-    if(!empty($_POST['nom']) && !empty($_POST['password'])) 
-    {
-      if($_POST['nom'] !== "Julien") 
-      {
-        $echo= 'Mauvais nom';
+    <?php 
+        if (!isset($_SESSION["Logged"]) && !$_SESSION["Logged"]) {
+            ?>
+                <form method="POST">
+                <label for="password">Nom:</label>
+                <input type="nom" name="nom" id="nom" value="" />
+                <p>
+                    <label for="password">Mot de passe:</label>
+                    <input type="password" name="password" id="password" value="" />
+                    <input type="submit" name="submit" value="connection" />
+                </p>
+            </form>
+            <?php
+if(isset($_POST["submit"])) 
+{
+  if(!empty($_POST['nom']) && !empty($_POST['password'])) {
+      if($_POST['nom'] !== "Julien") {
+          echo '<h1>Mauvais identifiant</h1>';
       }
-        elseif($_POST['password'] !== "1234") 
-      {  
-        $echo= 'Mauvais mot de passe';
+      else if($_POST['password'] !== "1234") {  
+          echo '<h1>Mauvais mot de passe</h1>';
+      } else {
+          $_SESSION["Logged"] = true;
+          header("Refresh:0");   
       }
-        else
-      {
-        session_start();
-        if (isset($_SESSION["Logged"]) && $_SESSION["Logged"]) {
-            echo "<h1>Bienvenue</h1>";
-            echo "<form action='' method='post'><button type='submit' name='logout'>Logout</button></form>";
+  }
+}
+        } else { 
+            ?>
+            <form method='post'>
+                <button type='submit' name='logout'>Logout</button>
+            </form>
+            <h1>Bienvenue</h1>
+            <?php
+    
             if (isset($_POST["logout"])) {
                 session_destroy();
                 header("Refresh:0");
-            } else {
-
-                echo '        
-                    <form action="" method="post">
-                        <p>
-                            <label for="user">Nom d\'utilisateur</label>
-                            <input type="text" name="user" require>
-                        </p>
-                        <p>
-                            <label for="password">Mot de passe</label>
-                            <input type="password" name="password" require>
-                        </p>
-                        <button type="submit" name="login">login</button>
-                    </form>
-                    '; 
             }
         }
-        
-}
-}
-}
-highlight_file(__FILE__)
+        ?>
+    </body>
+</html>
+<?php
+    highlight_file(__FILE__)
 ?>
-//
