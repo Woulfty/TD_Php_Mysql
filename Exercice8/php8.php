7518b191fa62a77aa221995baa129930829079d7
CARE <acare@la-providence.net>
2020-11-03 13:50:37 +0100

    exercice8

---
+++ Exercice8/php8.php
@@ -0,0 +1,27 @@
+<?php
+        session_start();
+        unset($_SESSION['count']);
+        ?>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>exercice7</title>
+    </head>
+    <body>
+    <form method="GET">
+            <input type="text"size="10" name="count">
+                <input type="submit" value="Envoyer"></p>
+            </form>
+            <?php
+        session_start();
+        if (!isset($_SESSION['count'])) {
+            $_SESSION['count'] = 0;
+        } else {
+            $_SESSION['count']++;
+        }
+        highlight_file(__FILE__)
+        ?>
+        <input type="button">
+    </body>
+</html>
